515fc0257f4b48ab91fff5cbc09c52d0e45c0583
yahyamachat <53411029+yahyamachat@users.noreply.github.com>
2019-07-31 06:38:47 +0100

    Create First_Code.c

---
+++ First_Code.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int array (int a)
+{
+return a*2;
+}
+
+int mul (int a , int b)
+{
+return a*b;
+}
+
+int main() {
+int i;
+int A[3];
+int B[3];
+int C[3];
+  
+
+  A[0] = 2;
+  A[1] = 3;
+  A[2] = 7;
+  B[0] = array(3);
+  B[1] = 3;
+  B[2] = 4;
+
+ for (i = 0; i < 3; i++) {
+ C[i]= mul(A[i],B[i]);
+ if (C[i] > 15) 
+    {
+	printf("%d\n", C[i]-15);
+	}
+	else
+	{
+	printf("%d\n", C[i]);
+	}
+}
+
+
+  return 0;
+}
