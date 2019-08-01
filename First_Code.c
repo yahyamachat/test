10cca1664b270a2e3f70cf72dff95d2ae49658ba
yahyamachat <machatyahya@gmail.comgit config --global user.email machatyahya@gmail.com>
2019-08-01 18:58:59 +0100

    Adding div function

--- First_Code.c
+++ First_Code.c
@@ -15,23 +15,31 @@ int add (int a , int b)
 return a+b;
 }
 
+/*This function is added through local (cloned) repository. */
+int div (int a , int b)
+{
+return a/b;
+}
+
 int main() {
 int i;
-int A[3];
-int B[3];
-int C[3];
+int A[4];
+int B[4];
+int C[4];
   
 
   A[0] = array(2);
   A[1] = mul(3,4);
   A[2] = add(7,2);
+  A[3] = div(29,4);
   B[0] = array(3);
   B[1] = mul(3,8);
   B[2] = add(4,3);
+  B[3] = div(37,9);
 /*The goal of this code is to multiplay 2 set of array*/
- for (i = 0; i < 3; i++) {
+ for (i = 0; i < 4; i++) {
  C[i]= mul(A[i],B[i]);
- if (C[i] > 15) 
+ if (C[i] > 80) 
     {
 	printf("%d\n", C[i]-15);
 	}
